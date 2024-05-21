f4f79cf4cef0beeb47a05ea1fca0cef5036e96a6
uditskaya-juliya <159793403+uditskaya-juliya@users.noreply.github.com>
2024-05-21 14:06:34 +0300

    Add files via upload

---
+++ 1_.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1
+{
+    struct Contact
+    {
+        private static int nextId = 1;
+        private readonly int id;
+        private string firstName;
+        private string lastName;
+        private string phoneNumber;
+        private string mail;
+
+        public Contact(string firstName, string lastName, string phoneNumber, string mail)
+        {
+            this.id = nextId++;
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.phoneNumber = phoneNumber;
+            this.mail = mail;
+        }
+
+        public void PrintInfo()
+        {
+            Console.WriteLine($"ID: {id}\tИмя: {firstName}\tФамилия: {lastName}\tТелефон: {phoneNumber}\tMail: {mail}");
+        }
+
+        public static void SortByLastName(Contact[] contacts)
+        {
+            for (int i = 0; i < contacts.Length - 1; i++)
+            {
+                for (int j = 0; j < contacts.Length - i - 1; j++)
+                {
+                    if (string.Compare(contacts[j].lastName, contacts[j + 1].lastName) > 0)
+                    {
+                        Swap(contacts, j, j + 1);
+                    }
+                }
+            }
+        }
+        public static void SortByFirstName(Contact[] contacts)
+        {
+            for (int i = 0; i < contacts.Length - 1; i++)
+            {
+                for (int j = 0; j < contacts.Length - i - 1; j++)
+                {
+                    if (string.Compare(contacts[j].firstName, contacts[j + 1].firstName) > 0)
+                    {
+                        Swap(contacts, j, j + 1);
+                    }
+                }
+            }
+        }
+        private static void Swap(Contact[] contacts, int index1, int index2)
+        {
+            Contact temp = contacts[index1];
+            contacts[index1] = contacts[index2];
+            contacts[index2] = temp;
+        }
+    }
+    class Program
+    {
+        static void Main()
+        {
+            Contact[] contacts = new Contact[]
+            {
+            new Contact("Михаил", "Иванов", "89653422134", "ivanov@mail.com"),
+            new Contact("Петр", "Петров", "897654321", "petrov@mail.com"),
+            new Contact("Анна", "Смирнова", "89674538890", "smirnova@mail.com"),
+            new Contact("Екатерина", "Сидорова", "8975643322", "katesidorova@mail.com"),
+            new Contact("Никита", "Рыбаков", "89746782212", "rybakov@mail.com")
+            };
+
+            Contact.SortByLastName(contacts);
+            Console.WriteLine("Сортировка по фамилии:");
+            foreach (var contact in contacts)
+            {
+                contact.PrintInfo();
+            }
+
+            Console.WriteLine();
+
+            Contact.SortByFirstName(contacts);
+            Console.WriteLine("Сортировка по имени:");
+            foreach (var contact in contacts)
+            {
+                contact.PrintInfo();
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ 2_10.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2
+{
+    abstract class Person
+    {
+        protected string firstName;
+        protected string lastName;
+
+        public string FirstName { get { return firstName; } }
+        public string LastName { get { return lastName; } }
+
+        public Person(string firstName, string lastName)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+        }
+
+        public abstract void PrintInfo();
+    }
+
+    class Employee : Person
+    {
+        private double salary;
+        private DateTime hireDate;
+
+        public double Salary { get { return salary; } }
+        public DateTime HireDate { get { return hireDate; } }
+
+        public Employee(string firstName, string lastName, double salary, DateTime hireDate) : base(firstName, lastName)
+        {
+            this.salary = salary;
+            this.hireDate = hireDate;
+        }
+
+        public override void PrintInfo()
+        {
+            Console.WriteLine($"Name: {FirstName} {LastName}\tSalary: {Salary}\tHire Date: {HireDate}");
+        }
+    }
+
+    class Counteragent : Person
+    {
+        private double contractCost;
+        private int contractDuration;
+
+        public double ContractCost { get { return contractCost; } }
+        public int ContractDuration { get { return contractDuration; } }
+
+        public Counteragent(string firstName, string lastName, double contractCost, int contractDuration) : base(firstName, lastName)
+        {
+            this.contractCost = contractCost;
+            this.contractDuration = contractDuration;
+        }
+
+        public override void PrintInfo()
+        {
+            Console.WriteLine($"Name: {FirstName} {LastName}\tContract Cost: {ContractCost}\tContract Duration: {ContractDuration} days");
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            Person[] people = new Person[]
+            {
+            new Employee("Anna", "D", 50000, new DateTime(2010, 5, 15)),
+            new Employee("Alice", "Smirn", 60000, new DateTime(2015, 8, 20)),
+            new Employee("Viktor", "Nikitin", 55000, new DateTime(2012, 3, 10)),
+            new Employee("Eva", "Timof", 52000, new DateTime(2018, 7, 25)),
+            new Employee("David", "Smirnov", 53000, new DateTime(2014, 9, 30)),
+            new Counteragent("Olya", "Larina", 100000, 365),
+            new Counteragent("Sophia", "Kotova", 80000, 180),
+            new Counteragent("Danila", "Krutov", 120000, 240),
+            new Counteragent("Lera", "Moroz", 90000, 300),
+            new Counteragent("Katya", "Volkova", 95000, 200)
+            };
+
+            for (int i = 0; i < people.Length - 1; i++)
+            {
+                for (int j = 0; j < people.Length - i - 1; j++)
+                {
+                    if (string.Compare(people[j].LastName, people[j + 1].LastName) > 0)
+                    {
+                        var temp = people[j];
+                        people[j] = people[j + 1];
+                        people[j + 1] = temp;
+                    }
+                }
+            }
+
+            Console.WriteLine("Employees:");
+            Console.WriteLine("Name\tSalary\tHire Date");
+            foreach (var person in people)
+            {
+                if (person is Employee)
+                {
+                    person.PrintInfo();
+                }
+            }
+            Console.WriteLine("\nCounteragents:");
+            Console.WriteLine("Name\tContract Cost\tContract Duration");
+            foreach (var person in people)
+            {
+                if (person is Counteragent)
+                {
+                    person.PrintInfo();
+                }
+            }
+            Console.WriteLine("\nAll People:");
+            Console.WriteLine("Name\tSalary/Contract Cost\tHire Date/Contract Duration");
+            foreach (var person in people)
+            {
+                person.PrintInfo();
+            }
+        }
+    }
+}
