f51419e7ddc59ef6b119fb7b8471dfeb103c5c31
uditskaya-juliya <159793403+uditskaya-juliya@users.noreply.github.com>
2024-05-29 16:41:36 +0300

    9 лр

--- MySerializerClass.cs
+++ MySerializerClass.cs
@@ -1,8 +1,6 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.IO;
-using System.Runtime.Serialization.Formatters.Binary;
-using System.Xml.Serialization;
-using Newtonsoft.Json;
 
 public abstract class MySerializeFormat
 {
@@ -14,55 +12,21 @@ public class JsonSerializeFormat : MySerializeFormat
 {
     public override void Serialize(object obj, string filePath)
     {
-        string json = JsonConvert.SerializeObject(obj, Formatting.Indented);
+        JsonSerializerSettings settings = new JsonSerializerSettings
+        {
+            TypeNameHandling = TypeNameHandling.All // Включить информацию о типах
+        };
+        string json = JsonConvert.SerializeObject(obj, Formatting.Indented, settings);
         File.WriteAllText(filePath, json);
     }
 
     public override object Deserialize(string filePath, Type type)
     {
         string json = File.ReadAllText(filePath);
-        return JsonConvert.DeserializeObject(json, type);
-    }
-}
-
-public class XmlSerializeFormat : MySerializeFormat
-{
-    public override void Serialize(object obj, string filePath)
-    {
-        XmlSerializer serializer = new XmlSerializer(obj.GetType());
-        using (StreamWriter writer = new StreamWriter(filePath))
+        JsonSerializerSettings settings = new JsonSerializerSettings
         {
-            serializer.Serialize(writer, obj);
-        }
-    }
-
-    public override object Deserialize(string filePath, Type type)
-    {
-        XmlSerializer serializer = new XmlSerializer(type);
-        using (StreamReader reader = new StreamReader(filePath))
-        {
-            return serializer.Deserialize(reader);
-        }
-    }
-}
-
-public class BinarySerializeFormat : MySerializeFormat
-{
-    public override void Serialize(object obj, string filePath)
-    {
-        BinaryFormatter formatter = new BinaryFormatter();
-        using (FileStream stream = new FileStream(filePath, FileMode.Create, FileAccess.Write, FileShare.None))
-        {
-            formatter.Serialize(stream, obj);
-        }
-    }
-
-    public override object Deserialize(string filePath, Type type)
-    {
-        BinaryFormatter formatter = new BinaryFormatter();
-        using (FileStream stream = new FileStream(filePath, FileMode.Open, FileAccess.Read, FileShare.Read))
-        {
-            return formatter.Deserialize(stream);
-        }
+            TypeNameHandling = TypeNameHandling.All // Использовать информацию о типах при десериализации
+        };
+        return JsonConvert.DeserializeObject(json, type, settings);
     }
 }
